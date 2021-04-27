747b7c5b37404f6436b03c7ad0884a3fb1941095
Carson Irons <>
2021-04-27 16:51:32 +0000

    Added code from U8 test FRQ 2

--- ProfNames.java
+++ ProfNames.java
@@ -2,8 +2,24 @@ public class ProfNames
 {
   public int countNames(String[][]nameArray, String lastName)
   {
-
-
-    return 0;
+//create counter
+int count=0;
+//traverse rows
+for(int r=0;r<nameArray.length;r++){
+//traverse columns
+for(int c=0; c<nameArray[0].length;c++){
+//check whether the current String contains target name (assuming the Strings inputed to the array are in //the correct format "lastName, FirstName"), if it does increment count
+if(nameArray[r][c].substring(0,lastName.length()).toLowerCase().equals(lastName.toLowerCase())){
+count++;
+//end if
+}
+//end nested loop
+}
+//end outter loop
+}
+//return count
+return count;
+//end method
   }
+    
 }
